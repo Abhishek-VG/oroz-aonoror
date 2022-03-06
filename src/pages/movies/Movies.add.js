cf2bce98e11346f95966ec74c839aeca24cda41d
Abhishek <abhishekvg@b2btesters.com>
2022-03-06 20:12:54 +0530

    Fix given

--- src/pages/movies/Movies.add.js
+++ src/pages/movies/Movies.add.js
@@ -2,6 +2,7 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import { FormControl } from "@mui/material";
 const MoviesAdd = () => {
   const [movieName, setmovieName] = React.useState("");
   const [movieDate, setmovieDate] = React.useState();
@@ -16,15 +17,14 @@ const MoviesAdd = () => {
     setmovieTickets(event.target.value);
   };
   const handleSubmit = (event) => {
-    event.stopPropagation();
-    event.nativeEvent.stopImmediatePropagation();
+    event.preventDefault();
     console.log(movieName, movieDate, movieTickets);
   };
   return (
     <div>
       <div>
         <Box
-          component="form"
+          component="div"
           sx={{
             "& .MuiTextField-root": { m: 1, width: "25ch" },
           }}
@@ -33,13 +33,12 @@ const MoviesAdd = () => {
         >
           <form onSubmit={handleSubmit}>
             <div>
-              <TextField
-                id="outlined-required"
-                label="Movie Name"
-                onChange={MovieNameHandler}
-              />
+            <TextField
+              id="outlined-required"
+              label="Movie Name"
+              onChange={MovieNameHandler}
+            />
             </div>
-
             <div>
               <TextField
                 id="outlined-required"
@@ -47,7 +46,6 @@ const MoviesAdd = () => {
                 onChange={MovieDateHandler}
               />
             </div>
-
             <div>
               <TextField
                 type="number"
@@ -57,8 +55,10 @@ const MoviesAdd = () => {
               />
             </div>
             <div>
-              <Button type="submit">Add Movie</Button>
             </div>
+            <Button type="submit" variant="contained" color="primary">
+              Add Movie
+            </Button>
           </form>
         </Box>
       </div>
