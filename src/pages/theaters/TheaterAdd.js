1e7eb5892bcf8642605d1fcab67ed0b762eeaf52
jyothi <jyothinh1995@gmail.com>
2022-03-08 17:43:12 +0530

    added

--- src/pages/theaters/TheaterAdd.js
+++ src/pages/theaters/TheaterAdd.js
@@ -1,6 +1,90 @@
 import React from "react";
-
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { FormControl } from "@mui/material";
+import Box from "@mui/material/Box";
+import { fetchApi } from "../utils/fetchMock";
+import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const TheaterAdd = () => {
-  return <p>im in theater add</p>;
+  let navigate = useNavigate();
+  const [theaterName, settheaterName] = React.useState("");
+  const [theaterDate, settheaterDate] = React.useState();
+  const [theaterLocation, settheaterLocation] = React.useState("");
+
+  const theaterNameHandler = (event) => {
+    settheaterName(event.target.value);
+  };
+
+  const theaterDatehandler = (event) => {
+    settheaterDate(event.target.value);
+  };
+
+  const theaterLocationHandler = (event) => {
+    settheaterLocation(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(theaterName, theaterDate, theaterLocation);
+    const theaterObj = {
+      theaterName,
+      theaterDate,
+      theaterLocation,
+    };
+    fetchApi("http://localhost:8080/theater ", {
+      method: "POST",
+      body: theaterObj,
+    })
+      .then((resp) => resp.json())
+      .then((obj) => navigate(`/theaters/list`));
+  };
+
+  return (
+    <div>
+      <div>
+        <Box
+          component="div"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <form id="ourform" onSubmit={handleSubmit}>
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="Theater Name"
+                onChange={theaterNameHandler}
+              />
+            </div>
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                type="date"
+                onChange={theaterDatehandler}
+              />
+            </div>
+            <div>
+              <TextField
+                required
+                type="text"
+                id="outlined-required"
+                label="Theater Location"
+                onChange={theaterLocationHandler}
+              />
+            </div>
+            <div></div>
+            <Button type="submit" variant="contained" color="primary">
+              Add Theater
+            </Button>
+          </form>
+        </Box>
+      </div>
+    </div>
+  );
 };
 export default TheaterAdd;
--- src/pages/theaters/TheaterList.js
+++ src/pages/theaters/TheaterList.js
@@ -1,6 +1,51 @@
 import React from "react";
-
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { fetchApi } from "../utils/fetchMock";
+import Paper from "@mui/material/Paper";
+import { useNavigate } from "react-router-dom";
 const TheaterList = () => {
-  return <p>im in theater Theater List</p>;
+  const [theaterList, settheaterList] = React.useState([]);
+  let navigate = useNavigate();
+  React.useEffect(() => {
+    fetchApi("http://localhost:8080/theater", { method: "GET" })
+      .then((resp) => resp.json())
+      .then((obj) => {
+        console.log(obj);
+        settheaterList(obj);
+      });
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {theaterList.map((row) => (
+            <TableRow
+              key={row.theaterName}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.theaterName}
+              </TableCell>
+              <TableCell align="right">{row.theaterDate}</TableCell>
+              <TableCell align="right">{row.theaterLocation}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 export default TheaterList;
