31053d93bbb2a634ae9488b0d951e5d557964e31
Abhishek <abhishekvg@b2btesters.com>
2022-03-16 15:26:25 +0530

    updated

--- src/pages/Toggle.js
+++ src/pages/Toggle.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { HooksLifeCycle } from "./HooksLifeCycle";
 import { ClassLifeCycle } from "./ClassLifeCycle";
+import { AsyncAwait } from "./AsyncAwait";
 
 export const Toggle = () => {
   const [count, setCount] = React.useState(0);
   const [off, setOff] = React.useState(false);
   return (
     <div>
-      {!off && <ClassLifeCycle count={count} />}<br/>
+      <AsyncAwait />
+      {/* {!off && <ClassLifeCycle count={count} />}<br/> */}
       <button
         onClick={() => {
           setCount(count + 1);
